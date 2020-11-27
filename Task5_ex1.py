4d458889f6e80255643c3f3b52c3496af49f1e70
Badrinathnooka <74898570+Badrinathnooka@users.noreply.github.com>
2020-11-27 11:39:56 +0530

    Add files via upload

---
+++ Task5_ex1.py
@@ -0,0 +1,13 @@
+def operators(a,b):
+    c=a+b
+    print("Addition of two numbers is ",c)
+    c=a-b
+    print("Subtraction of two numbers is ",c)
+    c=a*b
+    print("Multiplication of two numbers is ",c)
+    c=a/b
+    print("Division of two numbers is ",c)
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+operators(a,b)
+    
---
+++ Task5_ex2.py
@@ -0,0 +1,6 @@
+
+def covid(temp=98):
+    patient_name = input("enter the patient name : ")
+    body_temp    =int(input("Enter the body temperature of patient :"))
+    print("The patient name is ",patient_name," the body temp is ",body_temp,".The normal body temperature is",temp)
+covid()
